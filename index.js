ef4d0d83da177bc28b17c47ac9550af7dd7cfa3f
Cameron Wilby <cwilby@sendlane.com>
2018-10-03 12:46:59 -0700

    Initial commit.

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const stripe = require('stripe')(process.env.STRIPE_KEY);
+const app = express();
+
+app.use(require('morgan')('dev'));
+app.use(require('body-parser').json());
+
+app.post('/stripe', (req, res) => {
+    const { token: source, amount, currency, description } = req.body;
+
+    stripe.charges.create({
+        amount,
+        currency,
+        description,
+        source
+    }).then(charge => {
+        console.log('Charge: ' + JSON.stringify(charge));
+        res.status(200).json(charge);
+    }).catch(error => {
+        res.status(500).json(error);
+    });
+});
+
+const port = process.env.PORT || 8081;
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}.`);
+});
